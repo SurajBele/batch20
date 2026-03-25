cc067cce1a79ff20a445679cf029090fe5133de2
suraj <surajbele51@gmail.com>
2026-03-25 08:51:52 +0530

    terraform added

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "myinstance" {
   key_name = "id_rsa"
   vpc_security_group_ids = [ "sg-0882b4fee72084654", ]
   tags = {
-    Terraform   = "true"
+    name  = "suraj"
     Environment = "dev"
   }
 }

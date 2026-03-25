db3b691dc4a569abad38abd7004a6c10f1b725a9
suraj <surajbele51@gmail.com>
2026-03-25 08:51:44 +0530

    terraform added

---
+++ terraform/sample.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "myinstance" {
+  ami = "ami-0f559c3642608c138"
+  instance_type = "t3.micro"
+  key_name = "id_rsa"
+  vpc_security_group_ids = [ "sg-0882b4fee72084654", ]
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}

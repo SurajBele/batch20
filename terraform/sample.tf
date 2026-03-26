cba659d0ab8d7aa556e6db6ea3b070129baa838e
suraj <surajbele51@gmail.com>
2026-03-26 08:44:36 +0530

    backend added

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,6 +1,12 @@
 provider "aws" {
   region = "ap-south-1"
 }
+terraform {
+  backend "s3" {
+    bucket = "terraform12123"
+    region = "ap-south-1"
+  }
+}
 resource "aws_instance" "myinstance" {
   ami = "ami-0f559c3642608c138"
   instance_type = "t3.micro"

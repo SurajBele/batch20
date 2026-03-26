4032f8f1dae2168a5cf2ecf85fc811ed4ffc8540
suraj <surajbele51@gmail.com>
2026-03-26 09:15:17 +0530

    variable and output block added

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.rg
 }
 terraform {
   backend "s3" {
@@ -11,9 +11,9 @@ terraform {
 resource "aws_instance" "myinstance" {
   ami = var.ami_id
   instance_type = var.instance_type_mumbai
-  key_name = "id_rsa"
-  vpc_security_group_ids = [ "sg-0882b4fee72084654", ]
-  availability_zone = "ap-south-1a"
+  key_name = var.key_name
+  vpc_security_group_ids = var.mysg
+  availability_zone = var.az
   tags = {
     name  = "suraj"
     Name = "myinstance"
@@ -21,14 +21,4 @@ resource "aws_instance" "myinstance" {
   }
 }
 
-variable "ami_id" {
-  default = "ami-0f559c3642608c138"
-  description = "ap-south-1_ami"
-}
-variable "instance_type_mumbai" {
-  default = "t3.micro"
-}
 
-output "public_ip" {
-  value = aws_instance.myinstance.public_ip
-}

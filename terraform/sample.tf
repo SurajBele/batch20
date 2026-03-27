ce24137e5ff05269f38bfda0903e8163faf07375
suraj <surajbele51@gmail.com>
2026-03-27 09:28:03 +0530

    vpc added

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -8,11 +8,24 @@ terraform {
     key = "tfstatefile"
   }
 }
+
+data "aws_security_groups" "mysg" {
+  filter {
+    name   = "vpc-id"
+    values = [asetaefdrrg12123]
+  }
+  filter {
+    name   = "group-name"
+    values = ["mysg"]
+  }
+}
+
+
 resource "aws_instance" "myinstance" {
   ami = var.ami_id
   instance_type = var.instance_type_mumbai
   key_name = var.key_name
-  vpc_security_group_ids = [ var.mysg ]
+  vpc_security_group_ids = [data.aws_security_groups.mysg.id]
   availability_zone = var.az
   tags = {
     name  = "suraj"

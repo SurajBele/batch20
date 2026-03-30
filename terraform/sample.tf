935d8d281eb3cc05fcc1601ef1cbb4e0a849f88c
suraj <surajbele51@gmail.com>
2026-03-30 09:13:26 +0530

    module added

--- terraform/sample.tf
+++ terraform/sample.tf
@@ -1,37 +1,37 @@
-provider "aws" {
-  region = var.rg
-}
-terraform {
-  backend "s3" {
-    bucket = "terraform12123"
-    region = "ap-south-1"
-    key = "tfstatefile"
-  }
-}
+# provider "aws" {
+#   region = var.rg
+# }
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform12123"
+#     region = "ap-south-1"
+#     key = "tfstatefile"
+#   }
+# }
 
-data "aws_security_groups" "mysg" {
-  filter {
-    name   = "vpc-id"
-    values = [asetaefdrrg12123]
-  }
-  filter {
-    name   = "group-name"
-    values = ["mysg"]
-  }
-}
+# data "aws_security_groups" "mysg" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [asetaefdrrg12123]
+#   }
+#   filter {
+#     name   = "group-name"
+#     values = ["mysg"]
+#   }
+# }
 
 
-resource "aws_instance" "myinstance" {
-  ami = var.ami_id
-  instance_type = var.instance_type_mumbai
-  key_name = var.key_name
-  vpc_security_group_ids = [data.aws_security_groups.mysg.id]
-  availability_zone = var.az
-  tags = {
-    name  = "suraj"
-    Name = "myinstance"
-    Environment = "dev"
-  }
-}
+# resource "aws_instance" "myinstance" {
+#   ami = var.ami_id
+#   instance_type = var.instance_type_mumbai
+#   key_name = var.key_name
+#   vpc_security_group_ids = [data.aws_security_groups.mysg.id]
+#   availability_zone = var.az
+#   tags = {
+#     name  = "suraj"
+#     Name = "myinstance"
+#     Environment = "dev"
+#   }
+# }
 
 
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -17,3 +17,18 @@ variable "rg" {
 variable "az" {
   default = "ap-south-1a"
 }
+variable "project" {
+  default = "cloud"
+}
+variable "env" {
+  default = "devops"
+}
+variable "vpc-cidr"{
+    default = "10.0.0.0/16"
+}
+variable "pvt_subnet_cidr" {
+  default = "10.0.0.0/24"
+}
+variable "pub_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
